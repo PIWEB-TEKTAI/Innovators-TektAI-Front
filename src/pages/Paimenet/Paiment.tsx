88d2a9d87daeda0baa56912e4e7b93ce04cae779
fatma1abid <fatma.abid@esprit.tn>
2024-05-05 16:26:12 +0100

    MailPaiment

--- src/pages/Paimenet/Paiment.tsx
+++ src/pages/Paimenet/Paiment.tsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ConnectedClientLayout from '../../layout/ConnectedClientLayout';
-import { loadStripe, Stripe } from '@stripe/stripe-js';
-import PaymentForm from './formpaiment'; 
+import { loadStripe } from '@stripe/stripe-js';
+import PaymentForm from './formpaiment';
 
 interface Pack {
-  _id: string; 
+  _id: string;
   name: string;
+  image: string;
   description: string;
   price: number;
 }
 
 const PaymentPage = () => {
   const stripePromise = loadStripe('pk_test_51PCMzNHTb56tjMDuH9mlRWNgGo94uv9xBBcqLA67OjhwCn1U3GM6D0hF6xBZ1BixF9Trikzz2pwydkpGyugQFymD00UL9exp8n');
+
+  const [selectedPackId, setSelectedPackId] = useState<string>('');
 
   const [premiumPack, setPremiumPack] = useState<Pack | null>(null);
   const [freemiumPack, setFreemiumPack] = useState<Pack | null>(null);
@@ -23,6 +26,9 @@ const PaymentPage = () => {
     type: '',
     price: 0,
   });
+
+  // Etat pour contrôler l'ouverture de la modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     axios.get('http://localhost:3000/paiment/packs')
@@ -38,66 +44,57 @@ const PaymentPage = () => {
 
   const handleSubscriptionSelection = (type: string, price: number) => {
     setSelectedSubscription({ type, price });
+    setIsModalOpen(true); // Ouvrir la modal lorsque l'utilisateur sélectionne un abonnement
   };
 
-  const proceedToPayment = async () => {
-    if (selectedSubscription.type && selectedSubscription.price > 0) {
-      // Récupérer le stripe instance depuis la promesse
-      const stripe = await stripePromise;
-
-      // Créer une session de checkout avec le prix du pack sélectionné
-      const response = await axios.post('http://localhost:3000/paiment/checkout-session', {
-        price: selectedSubscription.price,
-        type: selectedSubscription.type,
-      });
-
-      // Rediriger vers la page de paiement Stripe
-      if (response.data.sessionId) {
-        const result = await stripe?.redirectToCheckout({
-          sessionId: response.data.sessionId,
-        });
-
-        if (result?.error) {
-          console.error('Erreur lors de la redirection vers la page de paiement:', result.error.message);
-        }
-      } else {
-        console.error('Session ID non disponible dans la réponse du serveur.');
-      }
-    } else {
-      alert('Veuillez sélectionner un abonnement avant de procéder au paiement');
-    }
+  const closeModal = () => {
+    setIsModalOpen(false); // Fermer la modal
   };
 
   return (
     <ConnectedClientLayout>
-      <div className="flex justify-center">
-        <div className="w-full lg:w-1/2 px-4 py-8">
-          <h1 className="text-3xl font-bold mb-4">Choose Your Subscription Type</h1>
-          <div className="flex justify-between mb-4">
-            {premiumPack && (
-              <div className="flex-1 mx-2 bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-xl font-semibold mb-4">{premiumPack.name}</h2>
-                <p>{premiumPack.description}</p>
-                <p>Price: {premiumPack.price}</p>
+    <div className="flex justify-center">
+      <div className="w-full lg:w-3/4 px-4 py-8"> {/* Augmentation de la largeur de la carte */}
+        <h1 className="text-4xl font-bold text-center mb-8 text-blue-700">Choose Your Subscription Plan</h1> {/* Augmentation de la taille du titre et utilisation de la couleur bleue */}
+        <div className="flex flex-wrap justify-between"> {/* Utilisation de flex-wrap pour une disposition responsive */}
+          {premiumPack && (
+            <div className="w-full md:w-1/2 lg:w-1/3 mb-8"> {/* Utilisation de différentes largeurs pour différents écrans */}
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <img src={premiumPack.image} alt={premiumPack.name} className="w-full h-64 object-cover rounded-t-lg mb-4" /> {/* Utilisation de Tailwind pour rendre l'image plus grande */}
+                <h2 className="text-2xl font-semibold mb-4 text-blue-700">{premiumPack.name}</h2> {/* Utilisation de la couleur bleue */}
+                <p className="text-lg mb-4">{premiumPack.description}</p>
+                <p className="text-lg mb-4">Price: <span className="text-red-700 font-bold">{premiumPack.price} DT</span></p> {/* Utilisation de la couleur rouge et du gras pour le prix */}
                 <button onClick={() => handleSubscriptionSelection('Premium', premiumPack.price)} className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Choose Premium</button>
               </div>
-            )}
-            {freemiumPack && (
-              <div className="flex-1 mx-2 bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-xl font-semibold mb-4">{freemiumPack.name}</h2>
-                <p>{freemiumPack.description}</p>
-                <p>Price: {freemiumPack.price}</p>
-                <button onClick={() => handleSubscriptionSelection('Freemium', freemiumPack.price)} className="mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Choose Freemium</button>
+            </div>
+          )}
+          {freemiumPack && (
+            <div className="w-full md:w-1/2 lg:w-1/3 mb-8"> {/* Utilisation de différentes largeurs pour différents écrans */}
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <img src={freemiumPack.image} alt={freemiumPack.name} className="w-full h-64 object-cover rounded-t-lg mb-4" /> {/* Utilisation de Tailwind pour rendre l'image plus grande */}
+                <h2 className="text-2xl font-semibold mb-4 text-blue-700">{freemiumPack.name}</h2> {/* Utilisation de la couleur bleue */}
+                <p className="text-lg mb-4">{freemiumPack.description}</p>
+                <p className="text-lg mb-4">Price: <span className="text-red-700 font-bold">{freemiumPack.price} DT</span></p> {/* Utilisation de la couleur rouge et du gras pour le prix */}
+                <button onClick={() => handleSubscriptionSelection('Freemium', freemiumPack.price)} className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Choose Freemium</button>
               </div>
-            )}
-          </div>
-          <div className="text-center mt-8">
-            <h1 className="text-3xl font-bold mb-4">Test Payment</h1>
-            <PaymentForm price={selectedSubscription.price} />
-          </div>
+            </div>
+          )}
         </div>
+        {/* Conditionnellement afficher la modal */}
+        {isModalOpen && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+            <div className="bg-white p-8 rounded-lg w-full max-w-lg">
+              <h1 className="text-3xl font-bold mb-4">Payment Form</h1>
+              <PaymentForm price={selectedSubscription.price} />
+              <button onClick={closeModal} className="mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Close</button>
+            </div>
+          </div>
+        )}
       </div>
-    </ConnectedClientLayout>
+    </div>
+  </ConnectedClientLayout>
+  
+  
   );
 };
 
--- src/pages/Paimenet/formpaiment.tsx
+++ src/pages/Paimenet/formpaiment.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import axios from 'axios';
+
+import { useAuth } from '../../components/Auth/AuthProvider'; // Importer useAuth
 
 interface PaymentFormProps {
     price: number; // Ajoutez le type de la propriété price
@@ -9,8 +11,18 @@ interface PaymentFormProps {
 function PaymentForm({ price }: PaymentFormProps) { // Ajoutez les props comme arguments de la fonction
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [paymentSuccess, setPaymentSuccess] = useState(false); // Ajout de l'état pour gérer le succès du paiement
+    const [email, setEmail] = useState(''); // Ajout de l'état pour l'adresse e-mail
     const stripe = useStripe();
     const elements = useElements();
+    const { userAuth } = useAuth(); // Récupérer les informations d'authentification de l'utilisateur connecté
+
+    // Utiliser l'e-mail de l'utilisateur connecté comme valeur par défaut pour l'état email
+    useEffect(() => {
+        if (userAuth && userAuth.email) {
+            setEmail(userAuth.email);
+        }
+    }, [userAuth]);
 
     const handlePayment = async () => {
         setLoading(true);
@@ -41,9 +53,10 @@ function PaymentForm({ price }: PaymentFormProps) { // Ajoutez les props comme a
                 const response = await axios.post('http://localhost:3000/paiment/create-subscription', {
                     paymentMethodId: paymentMethod.id,
                     planId: 'price_1PCj4LHTb56tjMDu8rWQaMWp', // Remplacez par l'ID de votre plan Stripe
-                    email: 'fatma.abid@esprit.tn', // Remplacez par l'email de l'utilisateur
+                    email: email, // Utilisation de l'e-mail de l'utilisateur connecté
                 });
                 console.log('Réponse du backend:', response.data);
+                setPaymentSuccess(true); // Mettre à jour l'état pour indiquer le succès du paiement
             }
         } catch (error) {
             console.error('Erreur lors de la création de l\'abonnement:', error);
@@ -58,28 +71,43 @@ function PaymentForm({ price }: PaymentFormProps) { // Ajoutez les props comme a
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <div>
+            {paymentSuccess ? (
+                <p>Votre paiement a été effectué avec succès.</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    {/* Le champ email est désactivé car il est rempli automatiquement avec l'e-mail de l'utilisateur connecté */}
+                    <input
+                        type="email"
+                        placeholder="Adresse e-mail"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="block w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                        disabled
+                    />
+                    <CardElement
+                        options={{
+                            style: {
+                                base: {
+                                    fontSize: '16px',
+                                    color: '#424770',
+                                    '::placeholder': {
+                                        color: '#aab7c4',
+                                    },
+                                },
+                                invalid: {
+                                    color: '#9e2146',
+                                },
                             },
-                        },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe || loading}>
-                {loading ? 'Chargement...' : 'Payer'}
-            </button>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-        </form>
+                        }}
+                    />
+                    <button type="submit" disabled={!stripe || loading} className="block w-full p-2 mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-md focus:outline-none focus:bg-blue-600">
+                        {loading ? 'Chargement...' : 'Payer'}
+                    </button>
+                    {error && <div style={{ color: 'red' }}>{error}</div>}
+                </form>
+            )}
+        </div>
     );
 }
 
